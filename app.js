351c53f3f475e3b6901a4d1d425fc043b44d29dc
taniya76 <www.taniyamittalhgn@gmail.com>
2021-08-30 11:42:02 +0530

    changes

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(express.urlencoded({
 app.use(express.static("public"));
 
 
-// mongodb+srv://admin-taniya:SrvNiya767@cluster0.z8u2a.mongodb.net/todolistDB
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+// mongodb://localhost:27017/todolistDB
+mongoose.connect("mongodb+srv://admin-taniya:SrvNiya767@cluster0.z8u2a.mongodb.net/todolistDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
